b49eadb38d9767e724a614d2b8ac2eced47fc55e
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 00:23:29 +0200

    Comment database connection for now
    #3

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const config = require('./config');
 
-module.exports = () => {
-    mongoose.set('useCreateIndex', true);
+// module.exports = () => {
+//     mongoose.set('useCreateIndex', true);
     
-    return mongoose.connect(config.dbUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log('Database connected.');
-    });
-}
+//     return mongoose.connect(config.dbUrl, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false
+//     }, (err) => {
+//         if (err) {
+//             throw err;
+//         }
+//         console.log('Database connected.');
+//     });
+// }
