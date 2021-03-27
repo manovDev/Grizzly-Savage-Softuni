60251b5df1e532ae7871261126691c0379e6773b
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 12:14:02 +0200

    Add MainLayout component to Main component
    #10

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,10 +1,13 @@
+import MainLayout from '../layouts/MainLayout';
 import './Main.scss';
 
 const Main = () => {
     return (
+        <MainLayout>
             <main className="main-wrapper">
                 Main component
             </main>
+        </MainLayout>
     );
 }
 
