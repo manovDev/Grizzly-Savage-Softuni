6488667f38bf3d39167b1dd60fc36d6e6caa8484
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 13:00:25 +0200

    Create Header component
    #7

---
+++ client/src/components/shared/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-logo">
+                Logo
+            </div>
+            
+        </header>
+    );
+}
+
+export default Header;
