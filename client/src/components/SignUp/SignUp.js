9b960faaf36c6a61193b025812aaee9ea7156b00
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 14:01:28 +0200

    Create SignUp component
    #15

---
+++ client/src/components/SignUp/SignUp.js
@@ -0,0 +1,14 @@
+import MainLayout from '../layouts/MainLayout';
+import './SignUp.scss';
+
+const SignUp = () => {
+    return (
+        <MainLayout> 
+            <div className="sign-up">
+                <div className="sign-up-content">
+            </div>
+        </MainLayout>
+    );
+}
+
+export default SignUp;
