b02cd746bb3879054fd646c7475ed00cebdedefe
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 12:13:28 +0200

    Create Main component
    #10

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.scss';
+
+const Main = () => {
+    return (
+            <main className="main-wrapper">
+                Main component
+            </main>
+    );
+}
+
+export default Main;
