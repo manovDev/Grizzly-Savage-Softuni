98b3455e85888d6deb36d9b4f1cfec3f51ddc9a7
Ivo Manov <ivoivchov@gmail.com>
2021-03-27 13:54:33 +0200

    Add basic markup form, links, buttons
    #16

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import './SignIn.scss';
 
@@ -5,7 +6,23 @@ const SignIn = () => {
     return (
         <MainLayout> 
             <div className="sign-in">
-                Sign In
+                <div className="sign-in-content">
+                    <h1>Sign In</h1>
+                    <form>
+                        <label htmlFor="email">Email</label>
+                        <input id="email" name="email" type="email" placeholder="john@domain.com"/>
+
+                        <label htmlFor="password">Password</label>
+                        <input id="password" name="password" type="password"/>
+
+                        <button type="button">Sign In</button>
+                    </form>
+                    <div className="new-customer">
+                        New customer?
+                        <Link to="/sign-up">Sign Up</Link>
+                    </div>
+                    
+                </div>
             </div>
         </MainLayout>
     );
