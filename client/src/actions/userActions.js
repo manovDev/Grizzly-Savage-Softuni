599cd51827a2d66af5f64d82c57aa8b775e36d07
Ivo Manov <ivoivchov@gmail.com>
2021-03-29 00:11:41 +0300

    Add user actions for signIn
    #23

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,4 +1,18 @@
-import { signUp as singUpService } from '../services/userService';
+import {
+    SIGNIN,
+} from './actionTypes';
+
+import firebase from '../firebase';
+import { 
+    signUp as singUpService,
+    signIn as singInService,
+} from '../services/userService';
+
+export const signInSuccess = (userData) => ({
+    type: SIGNIN,
+    payload: userData,
+})
+
 
 export const signUp = (data) => async (dispatch) => {
     
@@ -10,4 +24,21 @@ export const signUp = (data) => async (dispatch) => {
     } else {
         return responseJSON;
     }
+}
+
+export const signIn = (email, password) => async (dispatch) => {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(firebaseRes => {
+                const uid = firebaseRes.user.uid;
+                const idToken = firebaseRes.user.za;
+                return singInService(uid, idToken);
+            })
+            .then(res => res.json())
+            .then(user => {
+                dispatch(signInSuccess({ user }))
+            })
+            .catch(err => {
+                return err;
+            });
+    
 }
