eb2b5a9c2422b49f7fbd8db19eb4724ed66f8028
Ivo Manov <ivoivchov@gmail.com>
2021-03-29 00:13:24 +0300

    Add user service for sign in and verifyUser
    #23

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,3 +4,12 @@ import { SERVER_ADDRESS } from '../env';
 export const signUp = (data) => {
     return request.post(`${SERVER_ADDRESS}/user/signup`, data);
 }
+
+export const signIn = (uid, idToken) => {
+    return request.post(`${SERVER_ADDRESS}/user/signin`, { uid }, idToken);
+}
+
+export const verifyUser = (uid, idToken) => {
+    return request.post(`${SERVER_ADDRESS}/user/verifyuser`, { uid }, idToken);
+}
+
