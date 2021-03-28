64e778fb8f6a35c9167bd870c89877a63639b220
Ivo Manov <ivoivchov@gmail.com>
2021-03-29 00:13:45 +0300

    Fix statement bug in store.js
    #23

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
 
-const initialState = {};
+import rootReducer from './reducers'
 
-const middleware = [thunk];
+const middlewares = [ thunk ];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const composeEnhancers = (
+            typeof window !== 'undefined' 
+            && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ) 
+            || compose;
+
+const store = createStore(rootReducer, composeEnhancers (
+    applyMiddleware(...middlewares)
+));
 
 export default store;
