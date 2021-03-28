6d90477de09ffa9117d767eb0bb80249ee32ceff
Ivo Manov <ivoivchov@gmail.com>
2021-03-28 19:04:07 +0300

    Create form state and handleChange which updates it
    #21

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import './SignUp.scss';
 
 const SignUp = () => {
+    // const history = useHistory();
+    const [formData, setFormData] = useState({
+        firstName: null,
+        lastName: null,
+        email: null,
+        password: null,
+    });
+
+    const handleChange = (e) => {
+            
+        setFormData((currentErrState) => ({
+            ...currentErrState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
     return (
         <MainLayout> 
             <div className="sign-up">
@@ -10,18 +28,18 @@ const SignUp = () => {
                     <h1>Sign Up</h1>
                     <form>
                         <label htmlFor="firstName">First Name</label>
-                        <input id="firstName" name="firstName" type="text" placeholder="John"/>
+                        <input id="firstName" name="firstName" type="text" onChange={handleChange} placeholder="John"/>
 
                         <label htmlFor="lastName">Last Name</label>
-                        <input id="lastName" name="lastName" type="text" placeholder="Wilson"/>
+                        <input id="lastName" name="lastName" type="text" onChange={handleChange} placeholder="Wilson"/>
 
                         <label htmlFor="email">Email</label>
-                        <input id="email" name="email" type="email" placeholder="john@domain.com"/>
+                        <input id="email" name="email" type="email" onChange={handleChange} placeholder="john@domain.com"/>
 
                         <label htmlFor="password">Password</label>
-                        <input id="password" name="password" type="password"/>
+                        <input id="password" name="password" onChange={handleChange} type="password"/>
 
-                        <button type="button">Sign Up</button>
+                        <button>Sign Up</button>
                     </form>
                     <div className="new-customer">
                         Already a customer?
