63b3fbe85600c3c6c83e218083eb25ef79476208
Ivo Manov <ivoivchov@gmail.com>
2021-03-29 00:46:05 +0300

    Map state to props & dispatch to props in AuthLinks | show diff buttons on auth state
    #25

--- client/src/components/shared/Header/AuthLinks/AuthLinks.js
+++ client/src/components/shared/Header/AuthLinks/AuthLinks.js
@@ -1,23 +1,44 @@
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { signOut } from '../../../../actions/userActions';
 import './AuthLinks.scss';
 
-const AuthLinks = () => {
+const AuthLinks = ({ user, signOut }) => {
+
+    const handleSignOut =  () => {
+        signOut();
+    }
+
     return (
         <div className="auth-links">
             <ul>
-                <li>
-                    <Link to="/sign-in">Sign In</Link>
-                </li>
-                <li>
-                    <div className="separator">|</div>
-                </li>
-                <li>
-
-                    <Link to="/sign-up">Sign Up</Link>
-                </li>
+                {
+                    !user ?
+                        <>
+                            <li>
+                            <Link to="/sign-in">Sign In</Link>
+                            </li>
+                            <li>
+                                <div className="separator">|</div>
+                            </li>
+                            <li>
+                                <Link to="/sign-up">Sign Up</Link>
+                            </li>
+                        </>
+                        : <Link to="/" onClick={handleSignOut}>Sign Out</Link>
+                }
+                
             </ul>
         </div>
     );
 }
 
-export default AuthLinks;
+const mapStateToProps = state => ({
+    user: state.user.user,
+})
+
+const mapDispatchToProps = {
+    signOut,
+}
+    
+export default connect(mapStateToProps, mapDispatchToProps)(AuthLinks);
