0cf4b97267adaa7e10dc12d62abeef548b626de5
Ivo Manov <ivoivchov@gmail.com>
2021-03-30 15:16:56 +0300

    Create Products component and connect it to redux
    #31

---
+++ client/src/components/Main/Products/Products.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getAll as getAllProducts } from '../../../actions/productActions';
+import ProductItem from './ProductItem';
+import './Products.scss'
+
+const Products = ({ products, getAllProducts }) => {
+    useEffect(() => {
+        getAllProducts();
+        
+    }, [getAllProducts]);
+
+    return (
+        <section className="products-wrapper">
+        </section>
+    );
+}
+
+const mapStateToProps = (state) => ({
+    user: state.user.user,
+    products: state.products.products,
+});
+
+const mapDispatchToProps = {
+    getAllProducts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
