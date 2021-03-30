35dc9ae6d93d756479583cb07c38a300965aef31
Ivo Manov <ivoivchov@gmail.com>
2021-03-31 01:00:01 +0300

    Style and refactor header
    #34

--- client/src/components/shared/Header/Header.js
+++ client/src/components/shared/Header/Header.js
@@ -1,16 +1,13 @@
 import { connect } from 'react-redux';
-import SearchProducts from './SearchProducts';
-import AuthLinks from './AuthLinks';
+
+import NavBar from './NavBar';
 import './Header.scss';
 
 function Header({ user }) {
     return (
         <header className="header">
-            <div className="header-logo">
-                Logo
-            </div>
-            <SearchProducts />
-            <AuthLinks user={user} />
+
+            <NavBar user={user} />
 
         </header>
     );
