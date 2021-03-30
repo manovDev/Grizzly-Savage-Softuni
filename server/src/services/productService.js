002ec45dc6be10e1fc2ab6dec91ca4c92f9ad765
Ivo Manov <ivoivchov@gmail.com>
2021-03-30 12:56:16 +0300

    Create product controller foredit
    #29

--- server/src/services/productService.js
+++ server/src/services/productService.js
@@ -6,6 +6,17 @@ const create = async ({ title, image, price, brand, model, description, qtty }) 
     return await newProduct.save();
 }
 
+const edit = async ({ _id, title, image, price, brand, model, description, qtty }) => {
+    const editedProduct = await Product.findOneAndUpdate(
+        {_id},
+        {title, image, price, brand, model, description, qtty},
+        {new: true}
+        );
+    
+    return editedProduct;
+}
+
 module.exports = {
     create,
+    edit
 }
