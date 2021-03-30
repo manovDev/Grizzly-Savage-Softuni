8cbdf6560b784b1549b4482c3e85edbf5cb5ac1d
Ivo Manov <ivoivchov@gmail.com>
2021-03-30 13:24:11 +0300

    Add try catch condition
    #31

--- server/src/services/productService.js
+++ server/src/services/productService.js
@@ -1,31 +1,47 @@
 const Product = require('../models/Product');
 
 const getAll = async () => {
-    const allProducts = await Product.find();
+    try {
+        const allProducts = await Product.find();
     
-    return await allProducts;
+        return await allProducts;
+    } catch (error) {
+        throw error;
+    }
 }
 
 const create = async ({ title, image, price, brand, model, description, qtty }) => {
-    const newProduct = new Product({ title, image, price, brand, model, description, qtty });
+    try {
+        const newProduct = new Product({ title, image, price, brand, model, description, qtty });
     
-    return await newProduct.save();
+        return await newProduct.save();
+    } catch (error) {
+        throw error;
+    }
 }
 
 const edit = async ({ _id, title, image, price, brand, model, description, qtty }) => {
-    const editedProduct = await Product.findOneAndUpdate(
-        {_id},
-        {title, image, price, brand, model, description, qtty},
-        {new: true}
-        );
-    
-    return editedProduct;
+    try {
+        const editedProduct = await Product.findOneAndUpdate(
+            {_id},
+            {title, image, price, brand, model, description, qtty},
+            {new: true}
+            );
+        
+        return editedProduct;
+    } catch (error) {
+        throw error;
+    }
 }
 
 const del = async ({ _id }) => {
-    const deletedProduct = await Product.findOneAndDelete({_id});
+    try {
+        const deletedProduct = await Product.findOneAndDelete({_id});
     
-    return deletedProduct;
+        return deletedProduct;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
