61f779899784a0cb4da32d140e996e296949d1d6
Ivo Manov <ivoivchov@gmail.com>
2021-03-30 15:17:22 +0300

    Create products reducer
    #31

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './userReducer';
+import productsReducer from './productReducer';
 
 export default combineReducers({
     user: userReducer,
+    products: productsReducer,
 });
