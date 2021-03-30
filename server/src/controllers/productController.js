b631884cbe024f053a4a11b4ccb8b9016e86e0e0
Ivo Manov <ivoivchov@gmail.com>
2021-03-30 13:10:37 +0300

    Create product controller for delete
    #29

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -5,12 +5,12 @@ const verifyIdToken = require('../middlewares/verifyIdToken');
 const { productService } = require('../services');
 
 
-//TOTO: Add verify on create if the request is made from an admin
+//TOTO: Add verify if the request is made from an admin
 router.post('/create-product', async (req, res) => {
     try {
         const productRecord = await productService.create(req.body);
     
-        return res.json(productRecord);
+        return res.status(200).json(productRecord);
     } catch (error) {
         return res.status(400).json({ error });
     }
@@ -20,7 +20,17 @@ router.put('/edit-product', async (req, res) => {
     try {
         const productRecord = await productService.edit(req.body);
     
-        return res.json(productRecord);
+        return res.status(200).json(productRecord);
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+});
+
+router.delete('/delete-product', async (req, res) => {
+    try {
+        const productRecord = await productService.del(req.body);
+    
+        return res.status(200).json(productRecord);
     } catch (error) {
         return res.status(400).json({ error });
     }
