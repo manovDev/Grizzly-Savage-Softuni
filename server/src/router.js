64242089fcd7de3e46a15509f4ccb840fb3a69e2
Ivo Manov <ivoivchov@gmail.com>
2021-03-31 22:13:27 +0300

    create path for brand
    #40

--- server/src/router.js
+++ server/src/router.js
@@ -6,5 +6,6 @@ const controllers = require('./controllers');
 router.use('/user', controllers.user);
 router.use('/product', controllers.product);
 router.use('/category', controllers.category);
+router.use('/brand', controllers.brand);
 
 module.exports = router;
