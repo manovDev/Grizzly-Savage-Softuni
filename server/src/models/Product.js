e9a96e9a9c9df726e44d7ba685b3ad3fc13eec8e
Ivo Manov <ivoivchov@gmail.com>
2021-03-31 19:22:47 +0300

    Create category field in product model
    #38

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -13,6 +13,11 @@ const ProductSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
     brand: {
         type: String,
         required: true
