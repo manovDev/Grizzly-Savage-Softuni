bf9febc626d36fd398eaa1f7eaea5fe4091672dc
Ivo Manov <ivoivchov@gmail.com>
2021-03-31 22:13:58 +0300

    brand controller index
    #40

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,9 +1,11 @@
 const user = require('./userController');
 const product = require('./productController');
 const category = require('./categoryController');
+const brand = require('./brandController');
 
 module.exports = {
     user,
     product,
     category,
+    brand,
 };
