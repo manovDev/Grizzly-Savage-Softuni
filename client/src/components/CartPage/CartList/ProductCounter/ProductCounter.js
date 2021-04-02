50f984850a59d38c731d88d13ba038336d7656e9
Ivo Manov <ivoivchov@gmail.com>
2021-04-02 04:06:08 +0300

    make functionality for product counter
    #46

--- client/src/components/CartPage/CartList/ProductCounter/ProductCounter.js
+++ client/src/components/CartPage/CartList/ProductCounter/ProductCounter.js
@@ -1,21 +1,48 @@
-import './ProductCounter.scss';
-import { useState } from 'react';
+import { connect } from 'react-redux';
+import { updateCart } from '../../../../actions/cartActions';
 
-const ProductCounter = ({ qtty }) => {
-    const [productCounter, setProductCounter] = useState(1);
+import { useState, useEffect } from 'react';
+import './ProductCounter.scss';
+
+const ProductCounter = ({ user, cart, productId, updateCart, productUnits, maxQtty }) => {
+    const [productCounter, setProductCounter] = useState({productUnits, lastAction: ''});
+    useEffect(() => {
+        if(productCounter.lastAction) {
+            updateCart(productCounter, productId);
+            setProductCounter(curr => {
+                return {
+                    ...curr,
+                    lastAction: ''
+                }
+            });
+        }
+        
+    }, [productCounter, updateCart, productId]);
 
     const handleCounter = (e) => {
         const {id} = e.target;
 
         switch (id) {
             case 'reduceCounter':
-                setProductCounter(currNum => {
-                    return currNum - 1;
+                if(productCounter.productUnits - 1 < 1) return;
+
+                setProductCounter(curr => {
+                    const newUnits = curr.productUnits - 1 === 0 ? 1 : curr.productUnits - 1;
+                    return {
+                        productUnits: newUnits,
+                        lastAction: 'reduce'
+                    }
                 });
                 break;
             case 'incCounter':
-                setProductCounter(currNum => {
-                    return currNum + 1;
+                if(productCounter.productUnits + 1 > maxQtty) return;
+
+                setProductCounter(curr => {
+                    const newUnits = curr.productUnits + 1 > maxQtty ? maxQtty : curr.productUnits + 1;
+                    return {
+                        productUnits: newUnits,
+                        lastAction: 'inc'
+                    }
                 });
                 break;
             default:
@@ -26,12 +53,11 @@ const ProductCounter = ({ qtty }) => {
     return (
         <div className="product-qtty">
             <button className="reduceCounter" id="reduceCounter" onClick={handleCounter}>-</button>
-
             <input
                 type="number"
                 min="1"
-                max={qtty}
-                value={productCounter}
+                max={maxQtty}
+                value={productCounter.productUnits}
                 readOnly/>
 
             <button className="incCounter" id="incCounter" onClick={handleCounter}>+</button>
@@ -39,4 +65,13 @@ const ProductCounter = ({ qtty }) => {
     );
 }
 
-export default ProductCounter;
+const mapStateToProps = (state) => ({
+    user: state.user.user,
+    cart: state.cart
+});
+
+const mapDispatchToProps = {
+    updateCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCounter);
