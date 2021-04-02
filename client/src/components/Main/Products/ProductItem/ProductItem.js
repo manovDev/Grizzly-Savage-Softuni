d489c8f5b207b9681743694f81db30d2219b226c
Ivo Manov <ivoivchov@gmail.com>
2021-04-02 04:05:48 +0300

    make functionality to add product to cart list
    #46

--- client/src/components/Main/Products/ProductItem/ProductItem.js
+++ client/src/components/Main/Products/ProductItem/ProductItem.js
@@ -1,14 +1,33 @@
+import { connect } from 'react-redux';
+import { useState } from 'react';
+
+import { addToCart } from '../../../../actions/cartActions';
+
 import { Link } from 'react-router-dom';
 import './ProductItem.scss';
 
 const ProductItem = ({
+    user,
+    cart, 
+    addToCart,
     productData: { 
         _id,
         title,
         image,
         price,
-    } 
+    }
 }) => {
+    const [counter, setCounter] = useState(1);
+
+    const incCounter = (cartProducts, productsId) => {
+        setCounter(curr => curr += 1);
+        
+    }
+
+    const handleAddToCart = () => {
+        incCounter(cart.products, _id);
+        addToCart(_id, counter);
+    }
 
     return (
         <div className="product-item" data-id={_id}>
@@ -22,10 +41,18 @@ const ProductItem = ({
                     {title}
                 </ Link>
                 <div className="product-price">${price.toFixed(2)}</div>
-                <button className="add-to-cart-btn">Add To Cart</button>
+                <button className="add-to-cart-btn" onClick={handleAddToCart}>Add To Cart</button>
             </div>
         </div>
     );
 }
+const mapStateToProps = (state) => ({
+    user: state.user.user,
+    cart: state.cart
+});
 
-export default ProductItem;
+const mapDispatchToProps = {
+    addToCart,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductItem);
