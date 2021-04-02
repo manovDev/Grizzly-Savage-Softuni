b2d8ebe0260821c4b1bbe687a75b923dd9eecc04
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 00:33:00 +0300

    Add server route for orders - /order
    #49

--- server/src/router.js
+++ server/src/router.js
@@ -7,5 +7,6 @@ router.use('/user', controllers.user);
 router.use('/product', controllers.product);
 router.use('/category', controllers.category);
 router.use('/brand', controllers.brand);
+router.use('/order', controllers.order);
 
 module.exports = router;
