37172c14010bbd9278b924afc9f2164c676756ba
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 00:32:33 +0300

    Create orderController for place-order
    #49

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,10 +2,12 @@ const user = require('./userController');
 const product = require('./productController');
 const category = require('./categoryController');
 const brand = require('./brandController');
+const order = require('./orderController');
 
 module.exports = {
     user,
     product,
     category,
     brand,
+    order,
 };
