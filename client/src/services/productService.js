da9c2f60372c0be9df2e767bfee7833e882f7550
Ivo Manov <ivoivchov@gmail.com>
2021-04-02 04:02:25 +0300

    fix bug on product service > getOne (client)
    #46

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -4,3 +4,7 @@ import { SERVER_ADDRESS } from '../env';
 export const getAll = () => {
     return request.get(`${SERVER_ADDRESS}/product/get-all`, null, null);
 }
+
+export const getOne = (productId) => {
+    return request.get(`${SERVER_ADDRESS}/product/${productId}`, null, null);
+}
