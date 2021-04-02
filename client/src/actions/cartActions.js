f0bfe8b2b6c2ced4ae3ad8292e99155786394182
Ivo Manov <ivoivchov@gmail.com>
2021-04-02 03:59:15 +0300

    add action for add to cart
    #46

---
+++ client/src/actions/cartActions.js
@@ -0,0 +1,33 @@
+import {
+    ADD_TO_CART,
+} from './actionTypes';
+
+import {
+    getOne as getOneProduct,
+} from '../services/productService';
+
+export const addToCartSuccess = (product) => ({
+    type: ADD_TO_CART,
+    payload: product
+})
+
+
+export const addToCart = (productId, productUnits) => async (dispatch) => {
+    try {
+        const res = await getOneProduct(productId);
+        const data = await res.json();
+
+
+        if (data.error) {
+            throw data.error;
+        }
+
+        if(data._id) {
+            data.units = productUnits;
+            dispatch(addToCartSuccess(data));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
