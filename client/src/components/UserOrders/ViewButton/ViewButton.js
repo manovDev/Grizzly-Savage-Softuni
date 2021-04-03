a090952449a23d4edf19c880fdd19764592d0c13
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 06:17:34 +0300

    Create ViewButton component
    #49

---
+++ client/src/components/UserOrders/ViewButton/ViewButton.js
@@ -0,0 +1,10 @@
+import { AiFillEye } from 'react-icons/ai';
+import './ViewButton.scss';
+
+const ViewButton = () => {
+    return (
+        <button className="view-button"><AiFillEye /></button>
+    );
+}
+
+export default ViewButton;
