374bb417086c09132a32143febfcaea37aa28de8
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 06:20:27 +0300

    create /get-all order controller (server)
    #49

--- server/src/controllers/orderController.js
+++ server/src/controllers/orderController.js
@@ -4,6 +4,16 @@ const router = Router();
 const verifyIdToken = require('../middlewares/verifyIdToken');
 const { orderService } = require('../services');
 
+router.get('/get-all', verifyIdToken, async (req, res) => {
+    try {
+        const ordersRecord = await orderService.getAll(req.body);
+    
+        return res.status(200).json(ordersRecord);
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+});
+
 router.post('/place-order', verifyIdToken, async (req, res) => {
     try {
         const orderRecord = await orderService.placeOrder(req.body);
