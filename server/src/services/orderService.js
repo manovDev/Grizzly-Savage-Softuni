726ac955b43666b1352a3845ea1a1b4ce711f0d2
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 04:35:15 +0300

    refactor orderService > placeOrder and add props
    #49

--- server/src/services/orderService.js
+++ server/src/services/orderService.js
@@ -1,8 +1,32 @@
 const Order = require('../models/Order');
 
-const placeOrder = async ({ firstName, lastName, phoneNumber, address, shippingCost, tax, products, totalPrice, qtty }) => {
+const placeOrder = async ({
+    firstName,
+    lastName,
+    phoneNumber,
+    address,
+    city,
+    postalCode,
+    country,
+    shippingCost,
+    tax,
+    products,
+    totalPrice,
+    qtty }) => {
     try {
-        const newOrder = new Order({ firstName, lastName, phoneNumber, address, shippingCost, tax, products, totalPrice, qtty });
+        const newOrder = new Order({
+            firstName,
+            lastName,
+            phoneNumber,
+            address,
+            city,
+            postalCode,
+            country,
+            shippingCost,
+            tax,
+            products,
+            totalPrice,
+            qtty });
     
         return await newOrder.save();
     } catch (error) {
