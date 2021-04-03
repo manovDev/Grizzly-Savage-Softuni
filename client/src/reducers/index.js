de3ff1ccee155b0ae95808310313b130cc525f5d
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 06:19:12 +0300

    create GET_ORDERS order reducer
    #49

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import productsReducer from './productReducer';
 import cartReducer from './cartReducer';
+import orderReducer from './orderReducer';
 
 export default combineReducers({
     user: userReducer,
     products: productsReducer,
     cart: cartReducer,
+    orders: orderReducer,
 });
---
+++ client/src/reducers/orderReducer.js
@@ -0,0 +1,23 @@
+import {
+    GET_ORDERS
+} from '../actions/actionTypes';
+
+const initialState = {
+    orders: []
+}
+
+const orderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ORDERS:
+            return {
+                ...state,
+                orders: action.payload
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
+
+export default orderReducer;
