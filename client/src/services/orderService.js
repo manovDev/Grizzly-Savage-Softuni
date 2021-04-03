ea5a1baecf5661df6f3d4db7068ce3ecca10eff3
Ivo Manov <ivoivchov@gmail.com>
2021-04-03 06:20:03 +0300

    create client orderService that fetches user orders
    #49

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -4,3 +4,7 @@ import { SERVER_ADDRESS } from '../env';
 export const placeOrder = (data, idToken) => {
     return request.post(`${SERVER_ADDRESS}/order/place-order`, data, idToken);
 }
+
+export const getAll = (idToken) => {
+    return request.get(`${SERVER_ADDRESS}/order/get-all`, null, idToken);
+}
