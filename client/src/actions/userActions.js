d6704c346aca3b803e09742453ecc1b79b9a8e60
Ivo Manov <ivoivchov@gmail.com>
2021-04-04 15:08:28 +0300

    on sign up save profile image name in user's db and upload their image in fire base
    #52

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -5,6 +5,8 @@ import {
 } from './actionTypes';
 
 import firebase from '../firebase';
+import { storage } from '../firebase';
+
 import { 
     signUp as singUpService,
     signIn as singInService,
@@ -24,10 +26,14 @@ export const verify = () => ({
     type: VERIFY,
 })
 
-export const signUp = (data) => async (dispatch) => {
+export const signUp = (data, profileImageFile) => async (dispatch) => {
     
     const response = await singUpService(data);
     const responseJSON = await response.json();
+
+    const ref = storage.ref(responseJSON._id + "/" + data.profileImage);
+    
+    await ref.put(profileImageFile);
 
     if (responseJSON.error) {
         return { error: responseJSON.error };
@@ -39,9 +45,11 @@ export const signUp = (data) => async (dispatch) => {
 export const verifyAuth = () => (dispatch) => {
     firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
+            
             const uid = user.uid;
             const idToken = user.za;
             const response = await verifyUser(uid, idToken);
+
             const userData = await response.json();
 
             if(userData._id) {
@@ -59,11 +67,16 @@ export const signIn = (email, password) => async (dispatch) => {
             .then(firebaseRes => {
                 const uid = firebaseRes.user.uid;
                 const idToken = firebaseRes.user.za;
+
                 return singInService(uid, idToken);
             })
             .then(res => res.json())
             .then(user => {
                 dispatch(signInSuccess({ user }))
+                const ref = storage.ref(user._id + "/");
+                var storageRef = ref.child(user.profileImage);
+
+                storageRef.getDownloadURL()
             })
             .catch(err => {
                 return err;
