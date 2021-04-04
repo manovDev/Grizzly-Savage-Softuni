c7c1c659e376b3506031488ef2d70c536aaf465a
Ivo Manov <ivoivchov@gmail.com>
2021-04-04 18:49:07 +0300

    Move slideshow component in MainLayout and show it only if the pathname is "/"
    #56

--- client/src/components/layouts/MainLayout/MainLayout.js
+++ client/src/components/layouts/MainLayout/MainLayout.js
@@ -1,11 +1,20 @@
+import SlideShow from '../../shared/SlideShow';
 import Header from '../../shared/Header';
 import './MainLayout.scss';
 
 const MainLayout = ({ children }) => {
 
+    function shallShowSlideshow() {
+        if (window.location.pathname === '/') {
+            return <SlideShow />;
+        }
+    }
+
     return (
         <div className="main-layout-wrapper">
             <Header />
+            
+            {shallShowSlideshow()}
 
             <div className="main-layout-content">
                 {children}
