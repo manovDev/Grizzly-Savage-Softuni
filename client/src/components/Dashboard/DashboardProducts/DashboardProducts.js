8697522fae4b2e4a3a2eef8b077c6cabacede485
Ivo Manov <ivoivchov@gmail.com>
2021-04-11 01:55:25 +0300

    Create and style dashboard products
    #80

---
+++ client/src/components/Dashboard/DashboardProducts/DashboardProducts.js
@@ -0,0 +1,14 @@
+import DashboardLayout from '../../layouts/DashboardLayout';
+import './DashboardProducts.scss';
+
+const DashboardProducts = () => {
+    return (
+        <DashboardLayout>
+            <section className="dashboard-products-wrapper">
+                <h1>Dashboard Products</h1>
+            </section>
+        </DashboardLayout>
+    );
+}
+
+export default DashboardProducts;
