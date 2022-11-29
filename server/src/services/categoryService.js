29913cdc7d485d128fca632cbe6005522b6d7821
Ivo Manov <ivoivchov@gmail.com>
2022-11-30 01:04:40 +0200

    fix

--- server/src/services/categoryService.js
+++ server/src/services/categoryService.js
@@ -1,52 +1,52 @@
-const Category = require('../models/Category');
+const Category = require("../models/Category");
 
 const getAll = async () => {
-    try {
-        const allCategories = await Category.find();
-    
-        return await allCategories;
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		const allCategories = await Category.find();
+
+		return await allCategories;
+	} catch (error) {
+		throw error;
+	}
+};
 
 const create = async ({ name }) => {
-    try {
-        const newCategory = new Category({ name });
-    
-        return await newCategory.save();
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		const newCategory = new Category({ name });
+
+		return await newCategory.save();
+	} catch (error) {
+		throw error;
+	}
+};
 
 const edit = async ({ _id, name }) => {
-    try {
-        const editedCategory = await Category.findOneAndUpdate(
-            {_id},
-            {name},
-            {new: true}
-            );
-        
-        return editedCategory;
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		const editedCategory = await Category.findOneAndUpdate(
+			{ _id },
+			{ name },
+			{ new: true }
+		);
+
+		return editedCategory;
+	} catch (error) {
+		throw error;
+	}
+};
 
 const del = async ({ _id }) => {
-    try {
-        const deletedCategory = await Category.findOneAndDelete({_id});
-    
-        return deletedCategory;
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		const deletedCategory = await Category.findOneAndDelete({ _id });
 
+		return deletedCategory;
+	} catch (error) {
+		throw error;
+	}
+};
+console.log("ok lets gooo");
 module.exports = {
-    getAll,
-    create,
-    edit,
-    del
-}
+	getAll,
+	create,
+	edit,
+	del,
+};
