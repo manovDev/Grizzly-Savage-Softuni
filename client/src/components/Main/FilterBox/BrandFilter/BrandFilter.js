aba76a58a3bb7c3de11dca171da9c144772d547c
Ivo Manov <ivoivchov@gmail.com>
2022-11-30 01:23:55 +0200

    fixes

--- client/src/components/Main/FilterBox/BrandFilter/BrandFilter.js
+++ client/src/components/Main/FilterBox/BrandFilter/BrandFilter.js
@@ -1,49 +1,59 @@
-import {  useContext, useState, useEffect } from 'react';
-import { Filter } from '../../../../contexts/Filter';
-import { getAll as getAllBrands } from '../../../../services/brandService';
-import './BrandFilter.scss';
+import { useContext, useState, useEffect } from "react";
+import { Filter } from "../../../../contexts/Filter";
+import { getAll as getAllBrands } from "../../../../services/brandService";
+import "./BrandFilter.scss";
 
 const BrandFilter = () => {
-    const { filter, setFilter } = useContext(Filter);
-    const [brands, setBrands] = useState("");
+	const { filter, setFilter } = useContext(Filter);
+	const [brands, setBrands] = useState("");
 
-    useEffect(() => {
-        getAllBrands()
-            .then(res => res.json())
-            .then(brands => {
-                setBrands(brands);
-            });
-    }, [brands, setBrands]);
+	useEffect(() => {
+		getAllBrands()
+			.then((res) => res.json())
+			.then((brands) => {
+				setBrands(brands);
+			});
+	}, []);
 
-    const handleBrandFilter = (e) => {
-        setFilter((curr) => {
-            return {
-                ...curr,
-                brand: e.target.value,
-            }
-        });
-    }
+	const handleBrandFilter = (e) => {
+		setFilter((curr) => {
+			return {
+				...curr,
+				brand: e.target.value,
+			};
+		});
+	};
 
-    return (
-        <div className="brand-filter-wrapper">
-            <p>Brand:</p>
+	return (
+		<div className="brand-filter-wrapper">
+			<p>Brand:</p>
 
-            <label className="brand-option">All Brands
-                <input type="radio" name="radio" value="" onChange={handleBrandFilter}/>
-                <span className="checkmark"></span>
-            </label>
-            {
-                brands
-                ? brands.map(brand => (
-                    <label key={brand._id} className="brand-option">{brand.name}
-                        <input type="radio" name="radio" value={brand.name} onChange={handleBrandFilter}/>
-                        <span className="checkmark"></span>
-                    </label>
-                ))
-                : ''
-            }
-        </div>
-    );
-}
+			<label className="brand-option">
+				All Brands
+				<input
+					type="radio"
+					name="radio"
+					value=""
+					onChange={handleBrandFilter}
+				/>
+				<span className="checkmark"></span>
+			</label>
+			{brands
+				? brands.map((brand) => (
+						<label key={brand._id} className="brand-option">
+							{brand.name}
+							<input
+								type="radio"
+								name="radio"
+								value={brand.name}
+								onChange={handleBrandFilter}
+							/>
+							<span className="checkmark"></span>
+						</label>
+				  ))
+				: ""}
+		</div>
+	);
+};
 
 export default BrandFilter;
--- client/src/components/Main/FilterBox/CategoryFilter/CategoryFilter.js
+++ client/src/components/Main/FilterBox/CategoryFilter/CategoryFilter.js
@@ -1,49 +1,59 @@
-import {  useContext, useState, useEffect } from 'react';
-import { Filter } from '../../../../contexts/Filter';
-import { getAll as getAllCategories } from '../../../../services/categoryService';
-import './CategoryFilter.scss';
+import { useContext, useState, useEffect } from "react";
+import { Filter } from "../../../../contexts/Filter";
+import { getAll as getAllCategories } from "../../../../services/categoryService";
+import "./CategoryFilter.scss";
 
 const CategoryFilter = () => {
-    const { filter, setFilter } = useContext(Filter);
-    const [categories, setCategories] = useState("");
+	const { filter, setFilter } = useContext(Filter);
+	const [categories, setCategories] = useState("");
 
-    useEffect(() => {
-        getAllCategories()
-            .then(res => res.json())
-            .then(categories => {
-                setCategories(categories);
-            });
-    }, [categories, setCategories]);
+	useEffect(() => {
+		getAllCategories()
+			.then((res) => res.json())
+			.then((categories) => {
+				setCategories(categories);
+			});
+	}, []);
 
-    const handleCategoryFilter = (e) => {
-        setFilter((curr) => {
-            return {
-                ...curr,
-                category: e.target.value,
-            }
-        });
-    }
+	const handleCategoryFilter = (e) => {
+		setFilter((curr) => {
+			return {
+				...curr,
+				category: e.target.value,
+			};
+		});
+	};
 
-    return (
-        <div className="category-filter-wrapper">
-            <p>Category:</p>
+	return (
+		<div className="category-filter-wrapper">
+			<p>Category:</p>
 
-            <label className="category-option">All Categories
-                <input type="radio" name="radio" value="" onChange={handleCategoryFilter}/>
-                <span className="checkmark"></span>
-            </label>
-            {
-                categories
-                ? categories.map(category => (
-                    <label key={category._id} className="category-option">{category.name}
-                        <input type="radio" name="radio" value={category.name} onChange={handleCategoryFilter}/>
-                        <span className="checkmark"></span>
-                    </label>
-                ))
-                : ''
-            }
-        </div>
-    );
-}
+			<label className="category-option">
+				All Categories
+				<input
+					type="radio"
+					name="radio"
+					value=""
+					onChange={handleCategoryFilter}
+				/>
+				<span className="checkmark"></span>
+			</label>
+			{categories
+				? categories.map((category) => (
+						<label key={category._id} className="category-option">
+							{category.name}
+							<input
+								type="radio"
+								name="radio"
+								value={category.name}
+								onChange={handleCategoryFilter}
+							/>
+							<span className="checkmark"></span>
+						</label>
+				  ))
+				: ""}
+		</div>
+	);
+};
 
 export default CategoryFilter;
