e543a636b60e0c539bf950efe04463a860ebc4c4
Ivo Manov <ivoivchov@gmail.com>
2022-11-30 01:10:47 +0200

    sad

--- server/src/services/orderService.js
+++ server/src/services/orderService.js
@@ -1,80 +1,81 @@
-const Order = require('../models/Order');
-const User = require('../models/User');
+const Order = require("../models/Order");
+const User = require("../models/User");
 
 const placeOrder = async ({
-    userId,
-    firstName,
-    lastName,
-    phoneNumber,
-    address,
-    city,
-    postalCode,
-    country,
-    shippingCost,
-    tax,
-    products,
-    totalPrice,
-    qtty }) => {
-    try {
+	userId,
+	firstName,
+	lastName,
+	phoneNumber,
+	address,
+	city,
+	postalCode,
+	country,
+	shippingCost,
+	tax,
+	products,
+	totalPrice,
+	qtty,
+}) => {
+	try {
+		const newOrder = new Order({
+			firstName,
+			lastName,
+			phoneNumber,
+			address,
+			city,
+			postalCode,
+			country,
+			shippingCost,
+			tax,
+			products,
+			totalPrice,
+			qtty,
+		});
 
-        const newOrder = new Order({
-            firstName,
-            lastName,
-            phoneNumber,
-            address,
-            city,
-            postalCode,
-            country,
-            shippingCost,
-            tax,
-            products,
-            totalPrice,
-            qtty
-        });
+		await User.findByIdAndUpdate(userId, {
+			$addToSet: {
+				orders: newOrder._id,
+			},
+		});
 
-        await User.findByIdAndUpdate(userId, {
-            $addToSet: {
-                orders: newOrder._id
-            }
-        });
+		return await newOrder.save();
+	} catch (error) {
+		throw error;
+	}
+};
 
-        return await newOrder.save();
-    } catch (error) {
-        throw error;
-    }
-}
-
+//asdsad
 const getAll = async () => {
-    try {
-        const allOrders = await Order.find();
+	try {
+		const allOrders = await Order.find();
 
-        return await allOrders;
-    } catch (error) {
-        throw error;
-    }
-}
+		return await allOrders;
+	} catch (error) {
+		throw error;
+	}
+};
 
 const getOne = async (_id) => {
-    try {
-        const order = await Order.findById(_id);
+	try {
+		const order = await Order.findById(_id);
 
-        return await order;
-    } catch (error) {
-        throw error;
-    }
-}
+		return await order;
+	} catch (error) {
+		throw error;
+	}
+};
 
 const patch = async (_id, data) => {
-    try {
-        return await Order.findByIdAndUpdate(_id, data, { new: true })
-    } catch (error) {
-        throw error;
-    }
-}
+	try {
+		return await Order.findByIdAndUpdate(_id, data, { new: true });
+	} catch (error) {
+		throw error;
+	}
+};
 
 module.exports = {
-    placeOrder,
-    getAll,
-    getOne,
-    patch
-}
+	placeOrder,
+	getAll,
+	getOne,
+	patch,
+};
